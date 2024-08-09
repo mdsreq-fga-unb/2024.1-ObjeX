25bd32337cae328a4760bdde4647209580353ad6
cwtshh <gucosta1719@gmail.com>
2024-08-09 14:16:21 -0300

    fix: Consertando Dockerfile e docker-compose

--- api/config/db.js
+++ api/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const DB_USER = process.env.MONGO_INITDB_ROOT_USERNAME;
 const DB_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
 
 const connectDb = async() => {
     try {
-        await mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@db:27017/`);
+        await mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@db:${DB_PORT}/`);
         console.log('Connected to database');
     } catch (error) {
         console.error('Error connecting to database', error);
